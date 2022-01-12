845fb427fff1c2dd35ff2fdebc84e0a7a67d0548
JAYitis <20IT097@charusat.edu.in>
2022-01-12 06:14:21 +0530

    added a delete functionality in todo

--- app.js
+++ app.js
@@ -2,13 +2,14 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const date = require(__dirname + "/date.js") //importing from date.js
 const mongoose = require("mongoose")
+const _ = require("lodash");
 
 // console.log(date());
 
 const app = express();
 
-let items = [];
-let workItems = [];
+// let items = [];
+// let workItems = [];
 
 // 
 app.set("view engine", "ejs");
@@ -37,15 +38,19 @@ const item3 = new Item ({
 
 const defaultItems = [item1, item2, item3];
 
+const listSchema = {
+    name: String,
+    items: [itemsSchema]
+}
 
+const List = mongoose.model("List", listSchema);
 
-
+// using from date.js
+//let day = date.dayTime(); // displays day date month year time  
+let day = date.dayDate(); // displays day date month year  
 
 app.get("/", function (req, res) {     
-    // using from date.js
-    //let day = date.dayTime(); // displays day date month year time  
-    let day = date.dayDate(); // displays day date month year   
-
+     
     Item.find({}, function(err, foundItems){
 
         if(foundItems.length === 0){
@@ -67,16 +72,49 @@ app.get("/", function (req, res) {
     
 });
 
+app.get("/:customListName", function(req,res){
+    const customListName = _.capitalize(req.params.customListName);
+
+    List.findOne({name: customListName}, function(err, foundList){
+        if(!err){
+            if(!foundList){
+                // Create a new list
+                const list = new List({
+                    name: customListName,
+                    items: defaultItems
+                });
+
+                list.save();
+                res.redirect("/" + customListName)
+            } else {
+                //Show an existing list
+                res.render("List", {listTitle: foundList.name, newListItems: foundList.items})
+            }
+        }
+    });
+
+    
+});
+
 app.post("/", function(req,res){
     
     const itemName = req.body.newItem;  //it gets the item entered on frontend 
+    const listName = req.body.list;
 
     const item = new Item({
         name: itemName
-    })
+    });
 
-    item.save();
-    res.redirect('/');
+    if (listName === day){
+        item.save();
+        res.redirect('/');
+    } else {
+        List.findOne({name : listName}, function(err, foundList){
+            foundList.items.push(item);
+            foundList.save();
+            res.redirect('/' + listName);
+        });
+    }
 
 
     // if (req.body.list === "Work"){
@@ -91,9 +129,9 @@ app.post("/", function(req,res){
 
 });
 
-app.get("/work", function(req,res){
-    res.render("list", ({listTitle: "Work Things", newListItems: workItems}))
-})
+// app.get("/work", function(req,res){
+//     res.render("list", ({listTitle: "Work Things", newListItems: workItems}))
+// })
 
 app.get("/about", function(req,res){
     res.render("about");
@@ -101,17 +139,30 @@ app.get("/about", function(req,res){
 
 app.post('/delete', function(req, res){
     const checkedItemId = req.body.checkbox;
+    const listName = req.body.listName;
 
-    Item.findByIdAndRemove( checkedItemId, function(err){
+    // console.log(listName);
+    if( listName === day){
+        Item.findByIdAndRemove( checkedItemId, function(err){
         if(!err){
             console.log("deleted successfully");
             res.redirect('/')
         }
-    })
+        });
+    } else {
+        List.findOneAndUpdate({name: listName}, {$pull: {items: {_id: checkedItemId}}}, function(err, foundList){
+            if(!err){
+                res.redirect("/" + listName);
+            }
+        });
+    }
 
-})
+    
+
+})    
 
 app.listen(process.env.PORT || 3000, function (req, res) {
     console.log("Server running on 3000");
 });
 // 
+
